97348dd1bcb56ab55fda4e5e902213325c50edf1
rancessanchez <sanxezco@gmail.com>
2022-05-27 11:29:06 -0300

    definicion de un objeto

---
+++ fundamentos/objeto.js
@@ -0,0 +1,17 @@
+// objeto vacio
+const prod1 = {}
+// definiendo nombre del objeto
+prod1.nombre = 'Celular Ultra Mega'
+// definiendo precio del objeto
+prod1.precio = 4998.90
+
+console.log(prod1)
+
+
+// otra manera de definir un objeto
+const prod2 = {
+    nombre: 'Camisa Polo',
+    precio: 79.90,
+}
+
+console.log(prod2)
