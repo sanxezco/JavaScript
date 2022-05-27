5f94dbe5844638f6b0c22ea1655c96a8376728b2
rancessanchez <sanxezco@gmail.com>
2022-05-27 10:21:31 -0300

    add file dados.js

---
+++ fundamentos/dados.js
@@ -0,0 +1,19 @@
+var quantidade = 10
+var preco = 6.4
+var texto = "texto"
+const PI = 3.14
+console.log(PI)
+
+let sms = "let tambien define una variable"
+console.log(sms)
+
+console.log(texto)
+console.log(quantidade)
+console.log(preco)
+
+let nome = "Caderno "
+let categoria = "Escolar"
+console.log("Produto: " + nome + "Categoria: " + categoria)
+
+console.log("Produto: " + nome 
+            + "Categoria: " + categoria)
