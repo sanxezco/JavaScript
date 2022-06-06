87febf5daa5f7555d56e3957c623201ec84a140d
rancessanchez <sanxezco@gmail.com>
2022-06-06 09:46:15 -0300

    Implementecion de middleware

---
+++ node/middlewares.js
@@ -0,0 +1,36 @@
+// middleware pattern (chain of responsibility)
+
+const passo1 = (ctx, next) => 
+{
+    ctx.valor1 = 'mid1'
+    next()
+}
+
+const passo2 = (ctx, next) =>
+{
+    ctx.valor2 = 'mid2'
+    next()
+}
+
+const passo3 = (ctx) =>
+{
+    ctx.valor3 = 'mid3'
+}
+
+// crear la funcion que va a ejecutar todos los middlewares, pasando como argumento o contexto 'ctx' y un conjunto de middlewares '...middlewares'
+
+const exec = (ctx, ...middlewares) =>
+{
+    const execPassos = (indice) =>
+    {
+        middlewares && indice < middlewares.length && middlewares[indice](ctx, () =>
+        {
+            execPassos(indice + 1)
+        })
+    }
+    execPassos(0)
+}
+
+const ctx = {}
+exec(ctx, passo1, passo2, passo3)
+console.log(ctx)
