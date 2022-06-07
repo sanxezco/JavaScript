08d467b1cfc889cf3887f073aa3577ee3b86f4fc
rancessanchez <sanxezco@gmail.com>
2022-06-07 18:23:47 -0300

    definicion y uso de formularios

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body)
+    res.send('<h1>Parabéns. Usuário cadastrado</h1>')
+})
+
+app.listen(3003)
